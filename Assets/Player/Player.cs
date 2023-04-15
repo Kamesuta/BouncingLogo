b98704878971f7ce2c390fd71b8712a187a479db
Kamesuta <kamesuta@gmail.com>
2023-04-15 14:13:38 +0900

    DVDロゴゲーム再現

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 force;
+    private Rigidbody2D rigid;
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        rigid.AddForce(force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        sprite.color = Color.HSVToRGB(Random.Range(0f, 1f), 1f, 1f);
+    }
+}
